eefb389b419d9fe59331ef0de1b72878add2f02d
19khaled88 <kkhaled88hasan@gmail.com>
2023-05-31 23:53:54 +0600

    setup husky lint-staged

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,14 @@
-import express, { Application } from 'express'
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
-const app : Application= express()
-
+const app: Application = express()
 
 //middleware
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
-
-
 //routes
-app.get("/test",(req,res)=>{
-    res.send("hello")
+app.get('/test', (req: Request, res: Response) => {
+  res.send('hello')
 })
-
-
 
 export default app
