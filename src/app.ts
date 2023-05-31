4959f8b65bbea703a4900f1167495df4a3d661f2
19khaled88 <kkhaled88hasan@gmail.com>
2023-05-31 08:46:48 +0600

    Done

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
 import express, { Application } from 'express'
+import cors from 'cors'
 const app : Application= express()
 
 
+//middleware
+app.use(cors())
+app.use(express.urlencoded({extended:true}))
+
+
+//routes
 app.get("/",(req,res)=>{
     res.send("hello")
 })
